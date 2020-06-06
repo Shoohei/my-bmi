07b8e90265be6a6f6a5c2045f1b6e50dd7abeeaa
Shoohei <rouillardtonyp@gmail.com>
2020-06-06 10:31:37 +0900

    Finalize UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { ReactiveFormsModule } from "@angular/forms";
 
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { MatInputModule } from "@angular/material/input";
 import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatButtonModule } from "@angular/material/button";
 
 import { HttpClientModule } from "@angular/common/http";
 
@@ -24,7 +25,8 @@ import { BmiService } from "./bmi.service";
     ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   declarations: [AppComponent, BmiComponent],
   bootstrap: [AppComponent],
